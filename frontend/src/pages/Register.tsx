d2f503d61ab734f89315dea39be1174c9aa20931
Bharath <138557675+Bharath-anil@users.noreply.github.com>
2026-05-22 14:40:56 +0530

    implement frontend registration flow

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,55 @@
-function Register() {
-    return <h1>Register Page</h1>
+import { useState } from "react"
+import api from "../services/api"
+
+function Register(){
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    const handleRegister = async () =>{
+        try{
+            const response =await api.post("/register",{
+                username,
+                password
+            })
+            console.log(response.data)
+            alert("User registered Sucessfully")
+        }
+        catch (error){
+            console.log(error)
+            alert("Registeration failed")
+        }
+    }
+
+    return (
+        <div>
+            <h1>Register</h1>
+
+            <input
+                type="text"
+                placeholder="Enter User Name"
+                value={username}
+                onChange={(e)=> setUsername(e.target.value)}
+            />
+                
+            <br />
+            <br />
+
+            <input
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <br />
+            <br />
+
+            <button onClick={handleRegister}>Register</button>
+
+
+        </div>
+   )
 }
 
 export default Register
