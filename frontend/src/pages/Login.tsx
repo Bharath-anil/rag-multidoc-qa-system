0b37cde1d600c33afd88a6ef1a420f57381fab7d
Bharath <138557675+Bharath-anil@users.noreply.github.com>
2026-05-23 11:19:33 +0530

    implement frontend authentication flow

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,56 @@
+import { useState } from "react"
+import api from "../services/api"
+import { useNavigate } from "react-router-dom"
+
 function Login(){
-    return <h1>Login page</h1>
+
+    const [username,setUserName] =useState("")
+    const [password,setPassword] =useState("")
+
+    const navigate = useNavigate()
+    const handleLogin = async() =>{
+        try{
+            const response = await api.post("/login",{
+                username,
+                password
+            })   
+            
+            localStorage.setItem("token",response.data.access_token)
+            alert("login sucessful")    
+            navigate("/dashboard")
+        }
+        catch(error){
+            console.log(error)
+            alert("login failed")
+        }
+    }   
+
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <input
+            type="text"
+            placeholder="Enter the username"
+            value={username}
+            onChange={(e)=>setUserName(e.target.value)}
+            />
+            <br/>
+            <br/>
+
+            <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e)=> setPassword(e.target.value)}
+            />
+            <br/>
+            <br/>
+
+            <button onClick={handleLogin}>Login </button>
+
+        </div>
+    )
 }
 
 export default Login
